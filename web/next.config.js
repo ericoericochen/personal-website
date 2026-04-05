7b8e73d7e7ddfa1a56da27f567c7e13eb259395f
Eric Chen <deeplearningericchen@gmail.com>
2026-04-05 11:25:49 -0400

    fixing errors on vercel deployment

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,12 @@
 const { execSync } = require("child_process");
 
-const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+let commitHash;
+
+try {
+  commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+} catch {
+  commitHash = process.env.VERCEL_GIT_COMMIT_SHA;
+}
 
 module.exports = {
   env: {
